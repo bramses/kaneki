4e294f967d7d6fd3335759f69c6c78d2354081e7
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-05-08 15:05:21 -0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+![](https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/11/Kaneki-Ken-Featured-Image.jpg)
+
+# kaneki
+
+server that finds a file in obsidian and ports it to nextjs, allowing writing in obsidian
+being automatically pushed to server
---
+++ index.js
@@ -0,0 +1,139 @@
+/** 
+ * input: obsidian markdown file
+ * output: mdx file in the _posts directory
+ * 
+ * starts server if not already running
+ * open chrome and route to localhost:3000/projects/slug
+*/
+
+const { read } = require("gray-matter");
+const { spawn } = require( 'child_process' );
+const open = require('open');
+const { writeFileSync } = require('fs')
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = 3595;
+const OBSIDIAN_PATH = process.env.OBSIDIAN_PATH;
+const HOMEPAGE_PATH = process.env.HOMEPAGE_PATH;
+
+var path = require('path');
+
+app.use(express.json());
+app.use(express.static('public'))
+
+// define a route handler for the default home page
+app.get( "/", ( req, res, next ) => {
+    try {
+			// render the index template
+			var options = {
+                root: path.join(__dirname)
+            };
+            
+            var fileName = 'Hello.txt';
+            res.sendFile(fileName, options, function (err) {
+                if (err) {
+                    next(err);
+                } else {
+                    console.log('Sent:', fileName);
+                }
+            });
+		} catch (err) {
+      res.status(500).send(err);
+    }
+} );
+
+app.post('/update', async (req, res) => {
+    try {
+        const filePath = req.body.filePath;    
+        await upsertPost(filePath);
+        res.status(200).send(filePath);
+    } catch (err) {
+        console.error(err)
+        res.status(500).send(err);
+    }
+})
+
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+});
+
+
+
+
+const startHomepageServer = () => {
+    // spawn a new process
+    // change directory to HOMEPAGE_PATH
+    // run npm run dev
+    // run it in a shell
+    const server = spawn( `cd ${HOMEPAGE_PATH} && npm`, [ 'run', 'dev' ], {
+            shell: true,
+    });
+
+    server.stdout.on( 'data', ( data ) => {
+        console.log( data.toString() );
+    });
+
+    server.stderr.on( 'data', ( data ) => {
+        console.log( data.toString() );
+    });
+
+    server.on( 'close', ( code ) => {
+        console.log( `child process exited with code ${ code }` );
+    });
+
+    // when the server receives a SIGINT signal, log a message and kill the child process
+    server.on('SIGINT', () => {
+        console.log('SIGINT -- stopping server');
+        this.child.kill('SIGINT')
+    })
+
+}
+
+const openPostInChrome = (slug) => {
+    open("http://localhost:3000/projects/" + slug);
+}
+
+const upsertPost = async (filePath) => {
+    const markdown = read(filePath);
+    const frontmatter = markdown.data;
+    const markdownBody = markdown.content;
+    const slug = frontmatter.slug;
+
+    if (!slug) {
+        throw new Error('No slug found in frontmatter');
+    }
+
+    const file = `${HOMEPAGE_PATH}/_posts/${slug}.mdx`;
+    const event = new Date();
+
+    const mdx = `---
+title: ${frontmatter.title}
+featured: false
+published: true
+ogSlug: '${slug}'
+ogImageUrl: '${frontmatter.ogImageUrl}'
+coverImage: '${frontmatter.ogImageUrl}'
+date: '${event.toISOString()}'
+excerpt: '${frontmatter.excerpt}'
+---
+
+# ${frontmatter.title}
+
+${markdownBody}`;
+    writeFileSync(file, mdx);
+    return { slug };
+}
+
+const main = async () => {
+    const { slug } = await upsertPost(`${OBSIDIAN_PATH}/testblog.md`);
+    startHomepageServer();
+    openPostInChrome(slug);
+}
+
+main();
